04aeb6f48327390ccb3267f4399a9ef3e4ebc74b
Khath Khemanorak <khemanorak.k@codium.co>
2019-01-29 23:35:21 +0700

    Change dbname to template changeable

--- backend/config/settings/databases.py
+++ backend/config/settings/databases.py
@@ -2,6 +2,6 @@ import environ
 
 env = environ.Env()
 # Database configuration
-DEFAULT_DB_URL = "postgres://postgres:postgres@localhost:5432/hero_amw"
+DEFAULT_DB_URL = "postgres://postgres:postgres@localhost:5432/ChangeDBName"
 DATABASES = {'default': env.db("DATABASE_URL", default=DEFAULT_DB_URL)}
 DATABASES['default']['ATOMIC_REQUESTS'] = True
