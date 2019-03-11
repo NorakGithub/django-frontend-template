714773f48ded9e4de76745a9d82375e9dfb104b6
Khath Khemanorak <khemanorak.k@codium.co>
2019-03-11 16:31:29 +0700

    Added default ordering filter for rest setup

--- backend/config/settings/rest_framework.py
+++ backend/config/settings/rest_framework.py
@@ -10,6 +10,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
