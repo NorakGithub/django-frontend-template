b025929380211330c80a48c1aa1274371ad0d6dd
Khath Khemanorak <khemanorak.k@codium.co>
2019-03-11 15:44:31 +0700

    Moved api token related to api_urls

--- backend/config/api_urls.py
+++ backend/config/api_urls.py
@@ -1,8 +1,16 @@
+from django.urls import path
 from rest_framework import routers
+from rest_framework_jwt.views import (
+    obtain_jwt_token,
+    refresh_jwt_token,
+    verify_jwt_token
+)
 
 app_name = 'config'
 route = routers.DefaultRouter()
 
 urlpatterns = [
-
+    path('token-auth/', obtain_jwt_token),
+    path('token-refresh/', refresh_jwt_token),
+    path('token-verify/', verify_jwt_token),
 ] + route.urls
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,13 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, \
-    verify_jwt_token
 
 urlpatterns = [
-    path('api-token-auth/', obtain_jwt_token),
-    path('api-token-refresh/', refresh_jwt_token),
-    path('api-token-verify/', verify_jwt_token),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('config.api_urls', namespace='api')),
     path('admin/', admin.site.urls),
